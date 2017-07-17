21f44b966b2b164a515228cde327fed0625fd9d1
Jane <Jane@LAP-Jane.hfu-open.fu.hs-furtwangen.de>
2017-07-17 17:16:26 +0200

    feeeertiiig

--- Abschlussaufgabe/Abschlussaufgabe.js
+++ Abschlussaufgabe/Abschlussaufgabe.js
@@ -1,56 +1,69 @@
 var Abschlussaufgabe_Bild;
 (function (Abschlussaufgabe_Bild) {
-    window.addEventListener("load", bild);
-    Abschlussaufgabe_Bild.alleCookies = []; //Array wo die Cookies reingespeichert werden vom richtigen Ergebnis
-    Abschlussaufgabe_Bild.alleCookie = []; //Array wo die Cookies reingespeichert werden vom Ergebnis des Nutzers
-    ////////////////Rechenoperatoren plus Rechnung    
-    //a:deklaiert eine gerundete, variable Zahl bis 10
-    Abschlussaufgabe_Bild.a = Math.random() * 10; // variable zahl bis 10
-    Abschlussaufgabe_Bild.a = Math.round(Abschlussaufgabe_Bild.a); // rundet a auf 
-    //b:deklaiert eine gerundete, variable Zahl bis 10
-    Abschlussaufgabe_Bild.b = Math.random() * 10; // variable zahl bis 10
-    Abschlussaufgabe_Bild.b = Math.round(Abschlussaufgabe_Bild.b);
-    Abschlussaufgabe_Bild.ergebnis = Abschlussaufgabe_Bild.a + Abschlussaufgabe_Bild.b;
-    Abschlussaufgabe_Bild.nutzerAntwort = prompt("Grafzahl will Kr\u00fcmelmonster rechnen bei bringen und stellt zwei Teller mit Keksen hin." + "\r\n" + "Auf dem ersten Teller sind" + "  " + Abschlussaufgabe_Bild.a + "   " + "und auf dem zweiten Teller" + "  " + Abschlussaufgabe_Bild.b + "  " + "Kekse." + "\r\n" + "\r\n" + "Wie viele Kekse hat Kr\u00fcmelmonster insgesamt?" + "\r\n" + " ", Abschlussaufgabe_Bild.nutzerAntwort);
-    var nutzerAntwortNumber = parseInt(Abschlussaufgabe_Bild.nutzerAntwort);
-    // let falsch: HTMLElement = document.getElementById("Falsch");
-    //let richtig: HTMLElement = document.getElementById("Richtig");
-    var salat = document.getElementById("Salat");
-    ///////////////Entscheidet wie auf welche Arten die Nutzereingabe geteilt wird
-    if (isNaN(nutzerAntwortNumber)) {
-        Abschlussaufgabe_Bild.ausgabeVariation = "dumm";
+    var nutzerAntwortNumber;
+    //////////////Bildvariablen
+    var salat;
+    var falsch;
+    var richtig;
+    window.addEventListener("load", init);
+    function init() {
+        Abschlussaufgabe_Bild.a = Math.random() * 10; // variable zahl bis 10
+        Abschlussaufgabe_Bild.a = Math.round(Abschlussaufgabe_Bild.a);
+        Abschlussaufgabe_Bild.b = Math.random() * 10; // variable zahl bis 10
+        Abschlussaufgabe_Bild.b = Math.round(Abschlussaufgabe_Bild.b);
+        Abschlussaufgabe_Bild.ergebnis = Abschlussaufgabe_Bild.a + Abschlussaufgabe_Bild.b;
+        Abschlussaufgabe_Bild.nutzerAntwort = prompt("Grafzahl will Kr\u00fcmelmonster rechnen bei bringen und stellt zwei Teller mit Keksen hin." + "\r\n" + "Auf dem ersten Teller sind" + "  " + Abschlussaufgabe_Bild.a + "   " + "und auf dem zweiten Teller" + "  " + Abschlussaufgabe_Bild.b + "  " + "Kekse." + "\r\n" + "\r\n" + "Wie viele Kekse hat Kr\u00fcmelmonster insgesamt?" + "\r\n" + " ", Abschlussaufgabe_Bild.nutzerAntwort);
+        nutzerAntwortNumber = parseInt(Abschlussaufgabe_Bild.nutzerAntwort);
+        salat = document.getElementById("Salat");
+        falsch = document.getElementById("Falsch");
+        richtig = document.getElementById("Richtig");
+        Abschlussaufgabe_Bild.canvasEins = document.getElementById("richtigesErgebnis");
+        Abschlussaufgabe_Bild.crc2 = Abschlussaufgabe_Bild.canvasEins.getContext("2d");
+        Abschlussaufgabe_Bild.canvasZwei = document.getElementById("nutzerErgebnis");
+        Abschlussaufgabe_Bild.inhaltZwei = Abschlussaufgabe_Bild.canvasZwei.getContext("2d");
+        Abschlussaufgabe_Bild.alleCookies = []; //Array wo die Cookies reingespeichert werden vom richtigen Ergebnis
+        Abschlussaufgabe_Bild.alleCookie = []; //Array wo die Cookies reingespeichert werden vom Ergebnis des Nutzers
+        promptIt();
+        bild();
+        bildZwei();
     }
-    else if (nutzerAntwortNumber != Abschlussaufgabe_Bild.ergebnis) {
-        //salat.style.visibility = "visible";
-        Abschlussaufgabe_Bild.ausgabeVariation = "falsch";
-    }
-    else {
-        Abschlussaufgabe_Bild.ausgabeVariation = "richtig";
-    }
-    //////////////Entscheidet welcer Text in der Ausgabebox steht
-    switch (Abschlussaufgabe_Bild.ausgabeVariation) {
-        case "falsch":
-            Abschlussaufgabe_Bild.ausgabeText = "Die Antwort ist leider falsch." + "\r\n" + "Nun muss Kr\u00fcmelmonster Salat essen.";
-            salat.style.display = "block";
-            break;
-        case "dumm":
-            Abschlussaufgabe_Bild.ausgabeText = "Grafzahl verzweifelt, da dir nicht mal Ziffern gel\u00e4ufig sind?!";
-            //falsch.style.display = "block";
-            break;
-        case "richtig":
-            Abschlussaufgabe_Bild.ausgabeText = Abschlussaufgabe_Bild.ergebnis + "Kekse ist richtig! Da freut sich Grazahl und erstrecht Kr\u00fcmelmonster. Omnomnom!";
-            // richtig.style.display = "block";
-            break;
-        default:
-            Abschlussaufgabe_Bild.ausgabeText = "Uuups, es ist ein Fehler unterlaufen.";
-            break;
-    } //schlie�t switch
-    alert(Abschlussaufgabe_Bild.ausgabeText);
+    //////////////////AlertBox
+    function promptIt() {
+        var ausgabeVariation;
+        var ausgabeText;
+        ///////////////Entscheidet wie auf welche Arten die Nutzereingabe geteilt wird
+        if (isNaN(nutzerAntwortNumber)) {
+            ausgabeVariation = "dumm";
+        }
+        else if (nutzerAntwortNumber != Abschlussaufgabe_Bild.ergebnis) {
+            //salat.style.visibility = "visible";
+            ausgabeVariation = "falsch";
+        }
+        else {
+            ausgabeVariation = "richtig";
+        }
+        //////////////Entscheidet welcer Text in der Ausgabebox steht
+        switch (ausgabeVariation) {
+            case "falsch":
+                ausgabeText = "Die Antwort ist leider falsch." + "\r\n" + "Nun muss Kr\u00fcmelmonster Salat essen.";
+                salat.style.display = "block";
+                break;
+            case "dumm":
+                ausgabeText = "Grafzahl verzweifelt, da dir nicht mal Ziffern gel\u00e4ufig sind?!";
+                falsch.style.display = "block";
+                break;
+            case "richtig":
+                ausgabeText = Abschlussaufgabe_Bild.ergebnis + "Kekse ist richtig! Da freut sich Grazahl und erstrecht Kr\u00fcmelmonster. Omnomnom!";
+                richtig.style.display = "block";
+                break;
+            default:
+                ausgabeText = "Uuups, es ist ein Fehler unterlaufen.";
+                break;
+        } //schlie�t switch
+        alert(ausgabeText);
+    } //schlie�t promptIt
     ////////////////Erste Canvas mit sovielen Cookie wie es richtig ist
-    function bild(_event) {
-        var canvasEins;
-        canvasEins = document.getElementById("richtigesErgebnis");
-        Abschlussaufgabe_Bild.crc2 = canvasEins.getContext("2d");
+    function bild() {
         for (var i = 0; i < Abschlussaufgabe_Bild.ergebnis; i++) {
             var c = new Abschlussaufgabe_Bild.Cookie("black");
             Abschlussaufgabe_Bild.alleCookies.push(c);
@@ -59,10 +72,7 @@ var Abschlussaufgabe_Bild;
         } //schlie�t For-Schleife
     } //schlie�t function bild
     ////////////Zweiter Canvas mit so vielen Cookies wie Nutzer sagte
-    function bildZwei(_event) {
-        var canvasZwei;
-        canvasZwei = document.getElementById("nutzerErgebnis");
-        Abschlussaufgabe_Bild.inhaltZwei = canvasZwei.getContext("2d");
+    function bildZwei() {
         for (var i = 0; i < nutzerAntwortNumber; i++) {
             var k = new Abschlussaufgabe_Bild.CookieZwei("black");
             Abschlussaufgabe_Bild.alleCookie.push(k);
--- Abschlussaufgabe/Abschlussaufgabe.ts
+++ Abschlussaufgabe/Abschlussaufgabe.ts
@@ -1,79 +1,97 @@
 namespace Abschlussaufgabe_Bild {
-    window.addEventListener("load", bild);
 
-/////////////////CanvasVorbereitung
+    ////////////////Rechenoperatoren    
+    export let a: number;
+    export let b: number;
+    export let ergebnis: number;
+    //////////////AlertVorbereitung
+    export let nutzerAntwort: string;
+    let nutzerAntwortNumber: number;
+    //////////////Bildvariablen
+    let salat: HTMLImageElement;
+    let falsch: HTMLImageElement;
+    let richtig: HTMLImageElement;
+    /////////////////CanvasVorbereitung
+    export let canvasEins: HTMLCanvasElement;
+    export let canvasZwei: HTMLCanvasElement;
+    
     export let crc2: CanvasRenderingContext2D; //Inhalt des Canvas welcher das richtige Ergebnis anzeigt
     export let inhaltZwei: CanvasRenderingContext2D; //Inhalt des Canvas welcher Ergebnis des Nutzers anzeigt
-    export let alleCookies: Cookie[] = []; //Array wo die Cookies reingespeichert werden vom richtigen Ergebnis
-    export let alleCookie: CookieZwei[] = [];//Array wo die Cookies reingespeichert werden vom Ergebnis des Nutzers
-
-////////////////Rechenoperatoren plus Rechnung    
-    //a:deklaiert eine gerundete, variable Zahl bis 10
-    export let a: number = Math.random() * 10; // variable zahl bis 10
-    a = Math.round(a); // rundet a auf 
     
-    //b:deklaiert eine gerundete, variable Zahl bis 10
-    export let b: number = Math.random() * 10; // variable zahl bis 10
-    b = Math.round(b);
-    
-    //b und a werden zusammen addiert
-    export let ergebnis: number; // richtige ergebnis von a und b
-    ergebnis = a + b;
-       
-//////////////////AlertBox
-    export let nutzerAntwort: string; 
-    nutzerAntwort = prompt("Grafzahl will Kr\u00fcmelmonster rechnen bei bringen und stellt zwei Teller mit Keksen hin." + "\r\n" + "Auf dem ersten Teller sind" + "  " +  a +  "   " + "und auf dem zweiten Teller" + "  " + b + "  " + "Kekse." + "\r\n" + "\r\n" + "Wie viele Kekse hat Kr\u00fcmelmonster insgesamt?" + "\r\n" + " ", nutzerAntwort);
-    let nutzerAntwortNumber = parseInt(nutzerAntwort);
-    export let ausgabeVariation: string;
-    export let ausgabeText: string;
+    export let alleCookies: Cookie[]; //Array wo die Cookies reingespeichert werden vom richtigen Ergebnis
+    export let alleCookie: CookieZwei[];//Array wo die Cookies reingespeichert werden vom Ergebnis des Nutzers
 
-   // let falsch: HTMLElement = document.getElementById("Falsch");
-    //let richtig: HTMLElement = document.getElementById("Richtig");
-   let salat: HTMLElement = document.getElementById("Salat"); 
-    
-///////////////Entscheidet wie auf welche Arten die Nutzereingabe geteilt wird
-    if (isNaN(nutzerAntwortNumber)) { // wenn die NutzerAntwort keine Zahl ist
-        ausgabeVariation = "dumm";    
-        
-    }else if (nutzerAntwortNumber != ergebnis) {  // wenn die NutzerAntwort nicht das richtige Ergebnis ist
-        //salat.style.visibility = "visible";
-        ausgabeVariation = "falsch";        
-        
-    } else { // wenn die NutzerAntwort das richtige Ergebnis ist
-        ausgabeVariation = "richtig";
-        
-    }
 
-//////////////Entscheidet welcer Text in der Ausgabebox steht
-    switch (ausgabeVariation) {   
-        case "falsch": 
-            ausgabeText = "Die Antwort ist leider falsch." + "\r\n" +  "Nun muss Kr\u00fcmelmonster Salat essen.";
-            salat.style.display = "block";
-            
-            break;
-        case "dumm":
-            ausgabeText = "Grafzahl verzweifelt, da dir nicht mal Ziffern gel\u00e4ufig sind?!";
-            //falsch.style.display = "block";
-            break;
-        case "richtig":
-            ausgabeText = ergebnis + "Kekse ist richtig! Da freut sich Grazahl und erstrecht Kr\u00fcmelmonster. Omnomnom!";  
-           // richtig.style.display = "block";
-            break;
-        default:
-            ausgabeText = "Uuups, es ist ein Fehler unterlaufen.";
-            break;
-    }//schlieﬂt switch
+    window.addEventListener("load", init);
+    function init(): void {
+        a = Math.random() * 10; // variable zahl bis 10
+        a = Math.round(a);
+        b = Math.random() * 10; // variable zahl bis 10
+        b = Math.round(b)
+        ergebnis = a + b;
+        nutzerAntwort = prompt("Grafzahl will Kr\u00fcmelmonster rechnen bei bringen und stellt zwei Teller mit Keksen hin." + "\r\n" + "Auf dem ersten Teller sind" + "  " + a + "   " + "und auf dem zweiten Teller" + "  " + b + "  " + "Kekse." + "\r\n" + "\r\n" + "Wie viele Kekse hat Kr\u00fcmelmonster insgesamt?" + "\r\n" + " ", nutzerAntwort);
+        nutzerAntwortNumber = parseInt(nutzerAntwort);
+        salat = <HTMLImageElement>document.getElementById("Salat");
+        falsch = <HTMLImageElement>document.getElementById("Falsch");
+        richtig = <HTMLImageElement>document.getElementById("Richtig");
 
-    alert(ausgabeText);
- 
-////////////////Erste Canvas mit sovielen Cookie wie es richtig ist
-    function bild(_event: Event): void { // malt cookie.canvas
-        let canvasEins: HTMLCanvasElement;
         canvasEins = <HTMLCanvasElement>document.getElementById("richtigesErgebnis");
         crc2 = canvasEins.getContext("2d");
+        canvasZwei = <HTMLCanvasElement>document.getElementById("nutzerErgebnis");
+        inhaltZwei = canvasZwei.getContext("2d");
+        alleCookies = []; //Array wo die Cookies reingespeichert werden vom richtigen Ergebnis
+        alleCookie = [];//Array wo die Cookies reingespeichert werden vom Ergebnis des Nutzers
 
+        promptIt();
+        bild();
+        bildZwei();
+    }
+
+    //////////////////AlertBox
+    function promptIt(): void {
+        let ausgabeVariation: string;
+        let ausgabeText: string;
+        ///////////////Entscheidet wie auf welche Arten die Nutzereingabe geteilt wird
+        if (isNaN(nutzerAntwortNumber)) { // wenn die NutzerAntwort keine Zahl ist
+            ausgabeVariation = "dumm";
+
+        } else if (nutzerAntwortNumber != ergebnis) {  // wenn die NutzerAntwort nicht das richtige Ergebnis ist
+            //salat.style.visibility = "visible";
+            ausgabeVariation = "falsch";
+
+        } else { // wenn die NutzerAntwort das richtige Ergebnis ist
+            ausgabeVariation = "richtig";
+
+        }
+
+        //////////////Entscheidet welcer Text in der Ausgabebox steht
+        switch (ausgabeVariation) {
+            case "falsch":
+                ausgabeText = "Die Antwort ist leider falsch." + "\r\n" + "Nun muss Kr\u00fcmelmonster Salat essen.";
+                salat.style.display = "block";
+                break;
+            case "dumm":
+                ausgabeText = "Grafzahl verzweifelt, da dir nicht mal Ziffern gel\u00e4ufig sind?!";
+                falsch.style.display = "block";
+                break;
+            case "richtig":
+                ausgabeText = ergebnis + "Kekse ist richtig! Da freut sich Grazahl und erstrecht Kr\u00fcmelmonster. Omnomnom!";
+                richtig.style.display = "block";
+                break;
+            default:
+                ausgabeText = "Uuups, es ist ein Fehler unterlaufen.";
+                break;
+        }//schlieﬂt switch
+
+        alert(ausgabeText);
+
+    } //schlieﬂt promptIt
+    
+    
+    ////////////////Erste Canvas mit sovielen Cookie wie es richtig ist
+    function bild(): void { // malt cookie.canvas
         for (var i: number = 0; i < ergebnis; i++) { //
-            let c: Cookie = new Cookie("black"); 
+            let c: Cookie = new Cookie("black");
             alleCookies.push(c);
             console.log(c);
             c.drawCookie();
@@ -81,14 +99,10 @@ namespace Abschlussaufgabe_Bild {
 
         } //schlieﬂt For-Schleife
     }//schlieﬂt function bild
-    
-   
-////////////Zweiter Canvas mit so vielen Cookies wie Nutzer sagte
-    function bildZwei (_event: Event): void { // malt cookie.canvas
-        let canvasZwei: HTMLCanvasElement;
-        canvasZwei = <HTMLCanvasElement>document.getElementById("nutzerErgebnis");
-        inhaltZwei = canvasZwei.getContext("2d"); 
 
+
+    ////////////Zweiter Canvas mit so vielen Cookies wie Nutzer sagte
+    function bildZwei(): void { // malt cookie.canvas
         for (var i: number = 0; i < nutzerAntwortNumber; i++) { //
             let k: CookieZwei = new CookieZwei("black");
             alleCookie.push(k);
